88a6335bb306802a13b124516041217e1bcec139
Daniel James <daniel.james@modmed.com>
2022-01-20 18:08:30 -0500

    Added some empty controllers

---
+++ PatientTrackingBoardApp/Controllers/PatientController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientTrackingBoardApp.Services.Board;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PatientTrackingBoardApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly BoardService _boardService;
+
+        public PatientController(BoardService boardService)
+        {
+            _boardService = boardService;
+        }
+
+        [HttpGet("AddPatient/{firstName}/{lastName}/{sourceId}")]
+        public void AddPatient(string firstName, string lastName, string sourceId)
+        {
+        }
+
+        [HttpGet("DeletePatientById/{id}")]
+        public void DeletePatientById(string id)
+        {
+        }
+
+        [HttpGet("DeletePatientBySourceId/{sourceId}")]
+        public void DeletePatientBySourceId(string sourceId)
+        {
+        }
+
+        [HttpGet("UpdatePatientById/{id}/{firstName}/{lastName}/{sourceId}")]
+        public void UpdatePatientById(Guid id, string firstName, string lastName, string sourceId)
+        {
+        }
+
+        [HttpGet("UpdatePatientBySourceId/{sourceId}/{firstName}/{lastName}")]
+        public void UpdatePatientBySourceId(string sourceId, string firstName, string lastName)
+        {
+        }
+    }
+}
---
+++ PatientTrackingBoardApp/Controllers/VisitController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientTrackingBoardApp.Services.Board;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PatientTrackingBoardApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitController : ControllerBase
+    {
+        private readonly BoardService _boardService;
+
+        public VisitController(BoardService boardService)
+        {
+            _boardService = boardService;
+        }
+
+        [HttpPost("AddVisitByPatientId/{patientId}/{providerId}/{visitDate}")]
+        public void AddVisitByPatientId(string patientId, string providerId, string visitDate)
+        {
+        }
+
+        [HttpPost("AddVisitByPatientSourceId/{patientSourceId}/{providerId}/{visitDate}")]
+        public void AddVisitByPatientSourceId(string patientSourceId, string providerId, string visitDate)
+        {
+        }
+
+        [HttpPost("DeleteVisitByVisitId/{patientId}/{providerId}/{visitDate}")]
+        public void DeleteVisitByVisitId(string patientId, string providerId, string visitDate)
+        {
+        }
+    }
+}

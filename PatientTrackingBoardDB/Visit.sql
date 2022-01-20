40dc7b9daf82a9b8afc16f033448abf2493bebb6
Robert Mull <robert.mull@modmed.com>
2022-01-20 13:37:00 -0500

    Data model update

--- PatientTrackingBoardDB/Visit.sql
+++ PatientTrackingBoardDB/Visit.sql
@@ -1,15 +1,16 @@
 ﻿CREATE TABLE [dbo].[Visit] (
-    [Id]            UNIQUEIDENTIFIER NOT NULL,
-    [PatientId]     UNIQUEIDENTIFIER NOT NULL,
-    [ProviderId]    UNIQUEIDENTIFIER NOT NULL,
-    [VisitDate]     DATETIME         NOT NULL,
-    [VisitStatusId] UNIQUEIDENTIFIER NOT NULL,
-    [DateCreated]   DATETIME         CONSTRAINT [DF__Visit__DateCreated] DEFAULT (getdate()) NOT NULL,
-    [DateModified]  DATETIME         NOT NULL,
+    [Id]           UNIQUEIDENTIFIER NOT NULL,
+    [PatientId]    UNIQUEIDENTIFIER NOT NULL,
+    [ProviderId]   UNIQUEIDENTIFIER NOT NULL,
+    [VisitDate]    DATE             NOT NULL,
+    [DateCreated]  DATETIME         CONSTRAINT [DF__Visit__DateCreated] DEFAULT (getdate()) NOT NULL,
+    [DateModified] DATETIME         NOT NULL,
     CONSTRAINT [PK__Visit__3214EC074F1D5E09] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Visit_PatientId] FOREIGN KEY ([PatientId]) REFERENCES [dbo].[Patient] ([Id]),
     CONSTRAINT [FK_Visit_ProviderId] FOREIGN KEY ([ProviderId]) REFERENCES [dbo].[Provider] ([Id])
 );
+
+
 
 
 
